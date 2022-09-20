dd2052f42084de6956cf434dc5fe3e6eb19bc0c0
91093567 <91093567@mvusd.net>
2022-09-20 12:14:56 -0700

    Added main code in MaddisonBranch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("And a good day it is too!");
+        System.out.println("Maddison's Branch! WOOOOO");
+
+        String[] coolFrogNames = {"tomatoe", "squangle", "quingus", "bartholemew"};
+
+        System.out.println("\nCool Frog Names:");
+        for (int i = 0; i < coolFrogNames.length; i++)
+        {
+            System.out.println(coolFrogNames[i]);
+        }
     }
 }
