84159e839ba43f76d1f30ed8c506ce21c433a323
Jin <jindebest@gmail.com>
2022-09-20 12:13:56 -0700

    made a method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
         System.out.println("And a good day it is too!");
     }
+
+    public static void jinMethod()
+    {
+        System.out.println("I don't know to spell");
+    }
+
 }
