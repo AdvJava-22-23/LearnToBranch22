678d8eca42ed05703da570c3d388497c204c0ce5
91093861 <91093861@mvusd.net>
2022-09-20 12:14:06 -0700

    added janethMethod in JanethBranch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
         System.out.println("And a good day it is too!");
+    }
+
+    public static void janethMethod()
+    {
+        System.out.println("I am fabulous!");
     }
 }
